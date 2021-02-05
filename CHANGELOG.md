7b7f8cd515f9befa523a37ab95dded4b2a67be84
am401 <andras.marton@wpengine.com>
2021-02-04 23:21:39 -0600

    Added CHANGELOG.md and try block for dns.resolver request

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+## [ 0.1 ] - 2021-02-04
+### Added
+- `try`/`except` statement to wrap main block in to capture the `dns.resolver` action 
+- MailChannels SPF records
--- spf_records.py
+++ spf_records.py
@@ -6,15 +6,24 @@ def get_spf_record(domain):
     myAnswers = myResolver.resolve(domain,"TXT")
     validate_ip_regex = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}")
 
-    for rdata in myAnswers:
-        records = rdata.to_text().split()
-        for i in records:
-            x = re.findall(validate_ip_regex, i)
-            if x:
-                print(x[0])
+    try:
+        for rdata in myAnswers:
+            records = rdata.to_text().split()
+            for i in records:
+                x = re.findall(validate_ip_regex, i)
+                if x:
+                    print(x[0])
+    except dns.resolver.NoAnswer:
+        print(domain + ": No answer.")
+
+
 
 print("=" * 10 + " MailGun " + "=" * 10 + "\n")
 get_spf_record("spf2.mailgun.org")
 get_spf_record("spf1.mailgun.org")
 print("\n" + "=" * 10 + " Sendgrid " + "=" * 10 + "\n")
 get_spf_record("sendgrid.net")
+print("\n" + "=" * 10 + " MailChannel " + "=" * 10 + "\n")
+get_spf_record("spf1.mailchannels.net")
+get_spf_record("spf2.mailchannels.net")
+get_spf_record("spf3.mailchannels.net")
